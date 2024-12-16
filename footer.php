0b73791324c986eee89b63fda702583b821f6eb2
NguyenDoCong <tuoithodudoi.phungquan@gmail.com>
2024-12-16 14:23:43 +0700

    footer

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer id="footer">
+    <div class="copyright">
+        © <?php echo date('Y'); ?> <?php bloginfo('sitename'); ?>. <?php _e('All rights reserved', 'dec'); ?>. <?php _e('This website use WordPress', 'dec'); ?>
+    </div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+
+</html>
